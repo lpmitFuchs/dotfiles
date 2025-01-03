73f5c594ca401bf9574b6d21d7f8db0a546eb96a
Crisu1710 <m2rs@posteo.de>
2025-01-03 20:45:11 +0100

    add *

---
+++ hypr/hyprpaper-select.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+interval=300
+directory=~/Pictures/wallpapers
+monitor=`hyprctl monitors | grep Monitor | awk '{print $2}'`
+
+while true; do
+  if [ -d "$directory" ]; then
+      random_background=$(ls $directory/*.jpg | shuf -n 1)
+
+      hyprctl hyprpaper unload all
+      hyprctl hyprpaper preload $random_background
+      hyprctl hyprpaper wallpaper "$monitor, $random_background"
+      sleep $interval
+  fi
+done
