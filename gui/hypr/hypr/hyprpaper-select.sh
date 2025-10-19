d47a6614c72a5a6a2ab6592ad4c4459de9e58ca1
Crisu1710 <m2rs@posteo.de>
2025-10-19 21:31:11 +0200

    update

--- gui/hypr/hypr/hyprpaper-select.sh
+++ gui/hypr/hypr/hyprpaper-select.sh
@@ -6,7 +6,7 @@ monitor=`hyprctl monitors | grep Monitor | awk '{print $2}'`
 
 while true; do
   if [ -d "$directory" ]; then
-      random_background=$(ls $directory/*.jpg | shuf -n 1)
+      random_background=$(ls $directory/*.* | shuf -n 1)
 
       hyprctl hyprpaper unload all
       hyprctl hyprpaper preload $random_background
---
+++ gui/hypr/hypr/scripts/ChangeBlur.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+# Script for changing blurs on the fly
+
+STATE=$(hyprctl -j getoption decoration:blur:passes | jq ".int")
+
+if [ "${STATE}" == "2" ]; then
+	hyprctl keyword decoration:blur:size 2
+	hyprctl keyword decoration:blur:passes 1
+ 	notify-send -e -u low "Less Blur"
+else
+	hyprctl keyword decoration:blur:size 5
+	hyprctl keyword decoration:blur:passes 2
+  notify-send -e -u low "Normal Blur"
+fi
